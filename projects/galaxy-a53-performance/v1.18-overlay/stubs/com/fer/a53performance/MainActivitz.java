edb2e0660b19665f5b84411312e7dde87344df82
Damián Crispo <116601726+TiagoCrispo@users.noreply.github.com>
2026-08-18 22:14:01 -0300

    Add Galaxy v1.18 gallery superclass stub

---
+++ projects/galaxy-a53-performance/v1.18-overlay/stubs/com/fer/a53performance/MainActivitz.java
@@ -0,0 +1,7 @@
+package com.fer.a53performance;
+import android.app.Activity;
+import android.os.Bundle;
+public class MainActivitz extends Activity {
+    @Override protected void onCreate(Bundle state) { super.onCreate(state); }
+    @Override protected void onDestroy() { super.onDestroy(); }
+}
